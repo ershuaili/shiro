5bdf578cb5d8d4e2f3e2cb0e7828f6daef75d196
shuai <liershuai.cn@gmail.com>
2021-09-24 21:58:37 +0800

    master

--- src/main/java/com/example/shiro/controller/IndexController.java
+++ src/main/java/com/example/shiro/controller/IndexController.java
@@ -71,7 +71,7 @@ public class IndexController {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
-        return "redirect:login";
+        return "login";
     }
 
 
